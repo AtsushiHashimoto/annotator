58f82a1c69d214013d7f41f7775334cb2429b8ab
Atsushi Hashimoto <a_hasimoto@mm.media.kyoto-u.ac.jp>
2015-02-10 00:31:17 +0900

    webmへの変換が並列処理になっていないので2分探索を並列化

--- script/generate_blob_videos.rb
+++ script/generate_blob_videos.rb
@@ -3,11 +3,19 @@ require 'rubygems'
 require 'active_support'
 require 'parallel'
 
-WIN = false
+require 'optparse'
+opt = OptionParser.new
+
+threads = Parallel.processor_count
+opt.on('t','--threads') {|v| threads = v.to_i }
+raise "ERROR: threads must be more than 0" if threads < 1
+
+opt.parse!(ARGV)
+
+WIN = true
 GPU_DEV = 0
 if WIN then
 	def conv2winPath(path)
-		puts path
 		win_path = ""
 		if path =~ /\/cygdrive\/(.)\/(.*)/ then
 			win_path = "#{$1.upcase}:\\"
@@ -37,9 +45,15 @@ if ARGV.size < ARG_NUM then
 end
 
 if ARGV.size > ARG_NUM then
-	puts "Parallel Exe. CPU count => #{Parallel.processor_count}"
-	Parallel.each(ARGV, in_threads: Parallel.processor_count){|arg|
-		IO.popen("ruby #{__FILE__} #{arg}"){|io|
+	#puts "Parallel Exe. CPU count => #{Parallel.processor_count}"
+  ts = {}
+  ARGV.each{|arg|
+    t = (threads.to_f / ARGV.size)
+    ts[arg] = t>0 ? t : 1
+  }
+
+	Parallel.each(ARGV, in_threads: threads){|arg|
+		IO.popen("ruby #{__FILE__} #{arg} -t #{ts{arg}}"){|io|
 			io.each{|line|
 					puts line
 			}
@@ -143,48 +157,64 @@ end
 
 def trim(start_frame,end_frame, src_mov, dest_mov)
 	_dest_mov = dest_mov
-	if src_mov == dest_mov then
+	has_temp_src = (src_mov == dest_mov)
+	if has_temp_src then
 		ext_name = File.extname(dest_mov)
-		_dest_mov = File.dirname(dest_mov)+File.basename(dest_mov,ext_name) + "_temp" + ext_name
+		_dest_mov = File.dirname(dest_mov)+ "/" + File.basename(dest_mov,ext_name) + "_temp" + ext_name
 	end
 
 	if WIN then
 		src_mov = conv2winPath(src_mov)
+		_dest_mov_lin = _dest_mov
 		_dest_mov = conv2winPath(_dest_mov)
 	end
-	command = "ffmpeg -y -hwaccel_device #{GPU_DEV} -hwaccel auto  -i \"#{src_mov}\" -vf trim=start_frame=#{start_frame}:end_frame=#{end_frame},setpts=PTS-STARTPTS -c:v libvpx -an \"#{_dest_mov}\""
-	puts command
+
+	command = "ffmpeg -y -hwaccel_device #{GPU_DEV} -hwaccel auto  -i \"#{src_mov}\" -vf trim=start_frame=#{start_frame}:end_frame=#{end_frame},setpts=PTS-STARTPTS -c:v libvpx -an \"#{_dest_mov}\" > /dev/null 2> /dev/null"
+	#puts command
+	puts "TRIM: #{File.basename(src_mov)} => #{File.basename(_dest_mov)} [#{start_frame} to #{end_frame}]"
 	`#{command}`
-	if src_mov == dest_mov then
+	if has_temp_src then
 		ext_name = File.extname(dest_mov)
-		_dest_mov = File.dirname(dest_mov)+File.basename(dest_mov,ext_name) + "_temp" + ext_name
-		`mv #{_dest_mov} #{dest_mov}`
+		_dest_mov = File.dirname(dest_mov)+ "/" + File.basename(dest_mov,ext_name) + "_temp" + ext_name
+
+		command = "mv \"#{_dest_mov}\" \"#{dest_mov}\""
+		puts command
+		`#{command}`
 	end
 end
 
-
-def binary_trim(segments,s_off_set,src_mov,cam,cam_output_dir,end_frame)
-	return if segments.size == 1
+def binary_trim(segments,s_off_set,src_mov,cam,cam_output_dir,end_frame,para)
+	return if segments.size <= 1
 	mid = (segments.size()/2).to_i
 	dest_mov1 = "#{cam_output_dir}/#{"%07d"%(s_off_set+mid)}.webm"
 	first_frame = @timestamps[cam].search_video_index(segments[0][:observ_frame])
 	mid_frame = @timestamps[cam].search_video_index(segments[mid][:observ_frame])
 	raise "ERROR: segments: [#{segments.join(", ")}]" if first_frame >= end_frame or first_frame >= mid_frame
 
-	puts "binary_trim(#{cam}): [#{first_frame}...#{end_frame}] -> [#{first_frame}...#{mid_frame}] + [#{mid_frame}...#{end_frame}]"
+	puts "Binary Trim(#{cam}): [#{first_frame}...#{end_frame}] -> [#{first_frame}..#{mid_frame-1}] + [#{mid_frame}..#{end_frame-1}]"
 	trim(mid_frame-first_frame,end_frame-first_frame,src_mov,dest_mov1)
 	dest_mov2 = "#{cam_output_dir}/#{"%07d"%(s_off_set)}.webm"
-	trim(0,mid_frame-1,src_mov,dest_mov2)
-	binary_trim(segments[0...mid],s_off_set,    dest_mov2,cam,cam_output_dir,mid_frame-1)
-	binary_trim(segments[mid..-1],s_off_set+mid,dest_mov1,cam,cam_output_dir,end_frame)
+	trim(0,mid_frame-first_frame-1,src_mov,dest_mov2)
+  para = para * 2
+  if para <= threads/CAMERAS.size then
+    Process.fork{
+      binary_trim(segments[0..mid-1],s_off_set    ,dest_mov2,cam,cam_output_dir,mid_frame-1,para)
+    }
+  else
+    binary_trim(segments[0..mid-1],s_off_set    ,dest_mov2,cam,cam_output_dir,mid_frame-1,para)
+  end
+	binary_trim(segments[mid..-1],s_off_set+mid,dest_mov1,cam,cam_output_dir,end_frame,para)
 end
 
-
-CAMERAS.each{|cam|
-	next if cam != "cameraA"
+Parallel.each(CAMERAS, in_threads: Parallel.processor_count){|cam|
+#CAMERAS.each{|cam|
+	#next if cam != "cameraC"
 	cam_file = data_id_dir + "/#{cam}_#{data_id}.mp4"
 	cam_output_dir = "#{output_dir}/#{cam}"
-	binary_trim(segments,0,cam_file,cam,cam_output_dir,@timestamps[cam][-1][:observ_frame])
+
+	#####
+	#segments = segments[0..10]
+	binary_trim(segments,0,cam_file,cam,cam_output_dir,@timestamps[cam][-1][:observ_frame],1)
 }
 
 
