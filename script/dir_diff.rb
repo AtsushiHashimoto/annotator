fb20cb83d12beeca489bcaefb2b9156fc5b7ae0d
Atsushi Hashimoto <a_hasimoto@mm.media.kyoto-u.ac.jp>
2015-03-13 17:24:21 +0900

    add depth option

--- script/dir_diff.rb
+++ script/dir_diff.rb
@@ -5,14 +5,24 @@ require 'active_support'
 ARG_NUM = 2
 
 if ARGV.size < ARG_NUM then
-	STDERR.puts "USAGE: ruby #{__FILE__} dir1 dir2"
+	STDERR.puts "USAGE: ruby #{__FILE__} dir1 dir2 [depth]"
 	exit 1
 end
 
 dir1 = ARGV[0]
 dir2 = ARGV[1]
-file_list1 = `cd #{dir1};find . -print | sort`.split("\n").map{|v|v.strip}
-file_list2 = `cd #{dir2};find . -print | sort`.split("\n").map{|v|v.strip}
+
+depth = -1
+if ARGV.size > ARG_NUM then
+  depth = ARGV[2].to_i
+end
+
+exe_find = "find . -print"
+if depth>=0 then
+  exe_find += " -maxdepth #{depth}"
+end
+file_list1 = `cd #{dir1};#{exe_find} | sort`.split("\n").map{|v|v.strip}
+file_list2 = `cd #{dir2};#{exe_find} | sort`.split("\n").map{|v|v.strip}
 
 puts "only in #{dir1}"
 (file_list1-file_list2).each{|file| puts file}
