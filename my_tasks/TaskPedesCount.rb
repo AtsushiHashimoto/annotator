470c32efb05bd754c2258c0e73305baa4ad8dab0
Atsushi Hashimoto <a_hasimoto@mm.media.kyoto-u.ac.jp>
2015-08-02 23:41:27 +0900

    歩行者カウントタスク追加（作りかけ）

--- my_tasks/TaskPedesCount.rb
+++ my_tasks/TaskPedesCount.rb
@@ -1,8 +1,11 @@
-class Task2 < MyTask
+class TaskPedesCount < MyTask
   def initialize(config={})
     @task = self.class.name.underscore
     super(@task)
     @@config = generate_config(config)
+  end
+  def view_path_check
+    return "ERROR: This task (#{@task}) is not designed to double-check."#contents/#{@name}"
   end
 
   def load_timestamps(file)
@@ -10,7 +13,7 @@ class Task2 < MyTask
     File.open(file).each{|line|
       line = line.strip
       next if line.empty?
-      timestamps << parse_timestamps(line)
+      timestamps << parse_timestamp(line)
     }
     return timestamps
   end
@@ -41,7 +44,9 @@ blob_images
 　　　　┗567890.png
 =end
   def generate_tickets
-    all_data = Dir.glob(@@config[:data_path] + @@config[:glob_pattern]).sort
+    all_data = Dir.glob(@@config[:data_path] + @@config[:glob_pattern]).delete_if{|v|
+      !v.match(@@config[:data_id_regex])
+    }.sort
     # task_pedes_countの生成
     count = 0
 
@@ -50,18 +55,16 @@ blob_images
       next unless md
       data_id = md[1]
 
-      timestamps = load_timestamps
-      blob_paths = Dir.glob("#{data_path}/**#{@@config[:image_extension]}")
+      timestamps = load_timestamps("#{data_path}/#{@@config[:timestamp_file]}")
+      blob_paths = Dir.glob("#{data_path}/**/*#{@@config[:image_extension]}").map{|v|v.gsub!(@@config[:data_path],'')}
 
-      if timestamps.size != blob_paths then
+      if timestamps.size != blob_paths.size then
         STDERR.puts "ERROR: number of frames and timestamps are not equal."
         next
       end
 
       i = 0
       for blob_path in blob_paths do
-        timestamp = timestamps[i]
-        i = i+1
 
         md = blob_path.match(@@config[:blob_id_regex])
         unless md then
@@ -78,60 +81,65 @@ blob_images
         blob_path.gsub(@@config[:data_path],'')
         ticket = Ticket.new(_id: _id, blob_id: blob_id, task: @task, blob_path: blob_path, annotator:[])
         ticket['data_id'] = data_id
+        ticket['timestamp'] = timestamps[i]
+        ticket['frame'] = i
+        ticket['frame_num'] = timestamps.size
         unless ticket.save! then
           raise MyCustomError, "新規チケットの発行に失敗しました"
         end
+        i = i+1
       end
     end
 
     return count
 
   end
-  def load_nlp_result_csv(dir)
-    files = Dir.glob("#{dir}/*.csv")
-    candidates = Hash.new{|h,k| h[k] = []}
-    for file in files do
-      buf = File.open(file,'r').read.toutf8
-      words = buf.split("\n").map{|v|v.split(",")}.delete_if{|v|v.size < 4}
-      words.map!{|v|v[3].strip}
-      for key,array in @@config[:synonyms] do
-        for word in words do
-          next unless array.include?(word)
-          candidates[key] << word
-        end
-        candidates[key].uniq!
-      end
-    end
-    candidates
-  end
 
   def generate_meta_tags(ticket,current_task,user)
     meta_tags = {}
     meta_tags[:min_work_time] = @@util_funcs[:time2sec].call(@@config[:min_work_time]).to_s
 
-    synonyms = @@config[:synonyms]
+    meta_tags[:image_width] = @@config[:image_width]
+    meta_tags[:image_height] = @@config[:image_height]
 
-    meta_tags[:list_ingredient] = synonyms[:ingredient].to_json
-    meta_tags[:list_utensil   ] = synonyms[:utensil   ].to_json
-    meta_tags[:list_seasoning ] = synonyms[:seasoning ].to_json
-    meta_tags[:overview] = ticket[:blob_path] + "/" + @@config[:overview]
-
+    # 他の画像のタグを記録する
+    meta_tags[:past_pedestrians] = {}
+    tickets = Ticket.where(task:@task,blob_id:/#{ticket[:data_id]}.+/,completion:true)
+    other_mtasks = MicroTask.where(task:@task,blob_id:/#{ticket[:data_id]}.+/)
+    tickets.each{|t|
+      completed_mtasks = other_mtasks.where(blob_id:t.blob_id)
+      STDERR.puts "ERROR: empty completed_mtasks for blob_id:#{t.blob_id}"
+      completed_mtask = completed_mtasks[0]
+      next if completed_mtask['pedestrians'].empty?
+      meta_tags[:past_pedestrians][t['blob_id']] = completed_mtask['pedestrians']
+    }
     return MyTask::generate_meta_tags_base(meta_tags,ticket,current_task,user)
   end
+
   def parse_annotation(hash)
     annotation = {}
-    targets = [:ingredients,:utensils,:seasonings]
-    for tar in targets do
-      unless hash.include?(tar.to_s) then
-        raise 500, "空の入力欄(#{tar})があります"
-      end
-      annotation[tar] = hash[tar].split(",")
+    return annotation unless hash[:pedestrians] and !hash[:pedestrians].empty?
+
+    #画像のサイズで正規化しておく．
+    array =  JSON.parse(hash[:pedestrians]).uniq
+    for i in 0...array.length do
+      array[i] = array[i].with_indifferent_access
+      array[i][:x] = array[i][:x].to_f / @@config[:image_width]
+      array[i][:width] = array[i][:width].to_f / @@config[:image_width]
+      array[i][:y] = array[i][:y].to_f / @@config[:image_height]
+      array[i][:height] = array[i][:height].to_f / @@config[:image_height]
+      array[i][:gender]
+      # 他にも属性があれば，ここに追加
     end
+    annotation[:pedestrians] = array
+
     return annotation
   end
 
-  def view_path_check
-    return "contents/#{@name}"
+
+  def check_completion(ticket,mtasks)
+    STDERR.puts "WARNING: This task is not designed to double-check the annotation."
+    true
   end
 
 
